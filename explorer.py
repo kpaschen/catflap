27a1dab1aae7378c950c073447c56e420c45344b
Developer <contact@paschen.ch>
2020-04-05 18:50:40 +0200

    write labelfile for training

--- explorer.py
+++ explorer.py
@@ -20,13 +20,16 @@ class imageExplorer:
     def onContourMinSize(self, x):
         self.contourMinSize = x
 
-    def __init__(self):
+    def __init__(self, filename, labelfile):
         self.windowName = 'Processed image'
         self.cannyAperture = 3
         self.houghMinLL = 25
         self.houghMaxLG = 35
         self.houghStrength = 40
         self.contourMinSize = 30
+        self.filename = filename
+        self.coords = None
+        self.labelfile = labelfile
         cv2.namedWindow(self.windowName)
         cv2.createTrackbar('Canny aperture size', self.windowName, self.cannyAperture, 9, self.onCannyTrackbar)
         cv2.createTrackbar('Hough min line length', self.windowName, self.houghMinLL, 100, self.onHoughMinLLTrackbar)
@@ -35,10 +38,24 @@ class imageExplorer:
         cv2.createTrackbar('Contour min size', self.windowName, self.contourMinSize, 2000, self.onContourMinSize)
 
     def drawLinesOnImg(self, lines, img):
+        left = 400
+        right = 0
+        top = 240
+        bottom = 0
         for points in lines:
             print(points[0])
             p = points[0]
+            if p[0] < left:
+                left = p[0]
+            if p[1] < top:
+                top = p[1]
+            if p[2] > right:
+                right = p[2]
+            if p[3] > bottom:
+                bottom = p[3]
             cv2.line(img, (p[0],p[1]), (p[2],p[3]), (0, 0, 255), 1)
+        self.coords = (left, right, top, bottom)
+        print('left %d, right %d, top %d, bottom %d' % (left, right, top, bottom))
         
     def exploreImage(self, img):
         self.cur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -66,6 +83,21 @@ class imageExplorer:
                 self.colour = cv2.cvtColor(self.cur, cv2.COLOR_GRAY2BGR)
                 self.drawLinesOnImg(lines, self.colour)
                 cv2.imshow(self.windowName, self.colour)
+          elif pressed == 108: # 'l' for label
+            print('cat (a)rriving, (l)eaving, (n)o cat, (w)ait for next image?')
+            label = cv2.waitKey(0)
+            if label == 97:
+                descstr = 'cat arriving'
+            elif label == 108:
+                descstr = 'cat leaving'
+            elif label == 110:
+                descstr = 'no cat'
+            elif label == 119:
+                descstr = 'not sure what exactly'
+            else: descstr = 'an unknown object'
+            coordstr = ','.join(str(x) for x in self.coords) if self.coords else 'unknown'
+            print('image %s shows %s at %s' % (self.filename, descstr, coordstr))
+            self.labelfile.write('%s,%s,%s\n' % (self.filename, descstr, coordstr ))
           elif pressed == 111: # 'o' for contours
             contours, hierarchy = cv2.findContours(self.cur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             self.colour = cv2.cvtColor(self.cur, cv2.COLOR_GRAY2BGR)
--- listImages.py
+++ listImages.py
@@ -7,17 +7,15 @@ import sys
 
 from explorer import imageExplorer
 
-PICTURES_DIR='./images/'
-
 def eventkey(event):
   return event['timeofday'] + event['idx']
     
-def list_events_for_date(date_to_show):
+def list_events_for_date(imagedir, date_to_show):
   datestr = date_to_show.strftime('%Y%m%d')
   p = re.compile('(\d+)-' + datestr + '(\d+)-(\d+).jpg')
   events = {}
 
-  for file in os.listdir(PICTURES_DIR):
+  for file in os.listdir(imagedir):
     m = re.match(p, file)
     if m:
       event_id = m.group(1)
@@ -33,8 +31,8 @@ def list_events_for_date(date_to_show):
   return events
 
 
-def list_images(date_to_show):
-  events = list_events_for_date(date_to_show)
+def list_images(imagedir, date_to_show):
+  events = list_events_for_date(imagedir, date_to_show)
   if not events:
     print("No events for %s." % date_to_show.strftime('%Y-%m-%d'))
   print("There are %d events: " % len(events))
@@ -46,13 +44,13 @@ def list_images(date_to_show):
     print("first img %s at %s:%s:%s" % (x['filename'], x['timeofday'][0:2], x['timeofday'][2:4], x['timeofday'][4:6]))
 
 
-def show_image(date_to_show, idx):
-  ev = list_events_for_date(date_to_show)
+def show_image(imagedir, date_to_show, idx, labelfile):
+  ev = list_events_for_date(imagedir, date_to_show)
   if idx not in ev:
     print('no event %s for date %s' % (idx, date_to_show))
     return
   i = 0
-  img = cv2.imread(PICTURES_DIR + ev[idx][i]['filename'])
+  img = cv2.imread(imagedir + ev[idx][i]['filename'])
   windowname = '%s: %s' % (idx, date_to_show)
   cv2.imshow(windowname, img)
   while(1):
@@ -70,12 +68,12 @@ def show_image(date_to_show, idx):
       else:
          i -= 1
     elif pressed == 120: # 'x'
-      xp = imageExplorer()
+      xp = imageExplorer(ev[idx][i]['filename'], labelfile)
       xp.exploreImage(img)
     else:
       break
     print('showing image %d of %d' % (i, len(ev[idx])))
-    img = cv2.imread(PICTURES_DIR + ev[idx][i]['filename'])
+    img = cv2.imread(imagedir + ev[idx][i]['filename'])
     cv2.imshow(windowname, img)
   cv2.destroyAllWindows()
 
@@ -84,6 +82,8 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser('view camera images')
   parser.add_argument('--date', default=None, help='The day to show images for')
   parser.add_argument('--idx', default=None, help='The event to show images for')
+  parser.add_argument('--images', default='./images/', help='Where your image files are')
+  parser.add_argument('--labelfile', default='/tmp/catlabels.csv', help='Where to write labels')
   args = parser.parse_args()
   date_to_show = None
   if not args.date or args.date == 'today':
@@ -91,9 +91,11 @@ if __name__ == "__main__":
   else:
     date_to_show = datetime.strptime(args.date, "%Y-%m-%d").date()
   if args.idx is None:
-    list_images(date_to_show)
+    list_images(args.images, date_to_show)
   else:
-    show_image(date_to_show, args.idx)
-  
-  
-  
+      if not os.path.exists(args.labelfile):
+          with open(args.labelfile, 'w') as labelfile:
+              labelfile.write('filename,action,left,right,top,bottom')
+      with open(args.labelfile, 'a') as labelfile:
+          show_image(args.images, date_to_show, args.idx, labelfile)
+    
